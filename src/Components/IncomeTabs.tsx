bb65e44e3335bb806d5f78e476e828b345d42833
DanEA01 <gdea_18@hotmail.com>
2023-03-22 13:08:58 -0600

    fixed tab error

--- src/Components/IncomeTabs.tsx
+++ src/Components/IncomeTabs.tsx
@@ -5,7 +5,6 @@ import { useStyles } from '../Styles/styles';
 export const IncomeTabs = () => {
   
     const [tabIndex, setTabIndex] = useState(0);
-    const classes = useStyles();
   return (
     <Grid container spacing={3} sx={{marginBottom: '20px'}} alignItems="center" justifyContent="center">
         <Grid item>
